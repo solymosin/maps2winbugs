a732b9606e7e8af0d9bdb2e3f2037b1af51556a7
Solymosi Norbert <solymosi.norbert@gmail.com>
2015-08-03 08:10:45 +0200

    2.21

---
+++ plugin/editor.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ maps2WinBUGS
+                                 A QGIS plugin
+ a tool to facilitate data processing for
+Bayesian spatial modeling
+                              -------------------
+        begin                : 2015-07-31
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by Norbert Solymosi
+        email                : solymosi.norbert@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from PyQt4.QtCore import SIGNAL, Qt, QFile, QIODevice, QTextStream, QObject, QModelIndex
+from PyQt4.QtGui import QDialog, QProgressDialog, QFileDialog, QMessageBox, QApplication, QTextCursor, QStandardItemModel, QAbstractItemView, QItemSelectionModel
+from qgis.core import QGis, QgsVectorLayer, QgsFeature, QgsGeometry, QgsVectorDataProvider, QgsFields, QgsField, QgsFeatureRequest, QgsPoint
+
+from editor_dialog import Ui_editor_dialog
+
+
+class Dialog(QDialog, Ui_editor_dialog):         
+    def __init__(self):
+        """Constructor for the dialog.
+        
+        """
+        
+        QDialog.__init__(self)                               
+                        
+        self.setupUi(self)
+              
+
---
+++ plugin/editor_dialog.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'editor_dialog_base.ui'
+#
+# Created: Sat Aug  1 22:28:22 2015
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_editor_dialog(object):
+    def setupUi(self, editor_dialog):
+        editor_dialog.setObjectName(_fromUtf8("editor_dialog"))
+        editor_dialog.resize(533, 430)
+        self.gridLayout = QtGui.QGridLayout(editor_dialog)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.plainTextEdit = QtGui.QPlainTextEdit(editor_dialog)
+        self.plainTextEdit.setObjectName(_fromUtf8("plainTextEdit"))
+        self.gridLayout.addWidget(self.plainTextEdit, 0, 0, 1, 1)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.pushCancel = QtGui.QPushButton(editor_dialog)
+        self.pushCancel.setAutoDefault(False)
+        self.pushCancel.setObjectName(_fromUtf8("pushCancel"))
+        self.horizontalLayout.addWidget(self.pushCancel)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.pushSave = QtGui.QPushButton(editor_dialog)
+        self.pushSave.setAutoDefault(False)
+        self.pushSave.setDefault(True)
+        self.pushSave.setObjectName(_fromUtf8("pushSave"))
+        self.horizontalLayout.addWidget(self.pushSave)
+        self.gridLayout.addLayout(self.horizontalLayout, 1, 0, 1, 1)
+
+        self.retranslateUi(editor_dialog)
+        QtCore.QMetaObject.connectSlotsByName(editor_dialog)
+
+    def retranslateUi(self, editor_dialog):
+        editor_dialog.setWindowTitle(QtGui.QApplication.translate("editor_dialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushCancel.setText(QtGui.QApplication.translate("editor_dialog", "Cancel", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushSave.setText(QtGui.QApplication.translate("editor_dialog", "Save", None, QtGui.QApplication.UnicodeUTF8))
+
